59eaf67c6d1f81f3980cfe0a7db27af15011603e
John Eargle <jeargle@gmail.com>
2024-11-01 19:02:00 +0000

    #2 added examples for operations, broadcasting, and saving memory

--- test/torch_intro.py
+++ test/torch_intro.py
@@ -45,6 +45,74 @@ def ch2():
     X[:2, :] = 12
     print(f'X:')
     print(X)
+    print(f'exp(X):')
+    print(torch.exp(x))
+
+    # Operations
+    x = torch.tensor([1.0, 2, 4, 8])
+    y = torch.tensor([2, 2, 2, 2])
+    # x + y, x - y, x * y, x / y, x ** y
+    print(f'x:')
+    print(x)
+    print(f'y:')
+    print(y)
+    print(f'x + y:')
+    print(x + y)
+    print(f'x - y:')
+    print(x - y)
+    print(f'x * y:')
+    print(x * y)
+    print(f'x / y:')
+    print(x / y)
+    print(f'x ** y:')
+    print(x ** y)
+
+    X = torch.arange(12, dtype=torch.float32).reshape((3,4))
+    Y = torch.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
+    print('X:')
+    print(X)
+    print('Y:')
+    print(Y)
+    print('torch.cat((X, Y), dim=0):')
+    print(torch.cat((X, Y), dim=0))
+    print('torch.cat((X, Y), dim=1):')
+    print(torch.cat((X, Y), dim=1))
+    print('X == Y:')
+    print(X == Y)
+    print('X.sum():')
+    print(X.sum())
+
+    # Broadcasting
+    a = torch.arange(3).reshape((3, 1))
+    b = torch.arange(2).reshape((1, 2))
+    print('a:')
+    print(a)
+    print('b:')
+    print(b)
+    print('a + b:')
+    print(a + b)
+
+    # Saving memory
+    before = id(Y)
+    Y = Y + X
+    print('before:')
+    print(before)
+    print('Y:')
+    print(Y)
+    print(f'id(Y) == before: {id(Y) == before}')
+
+    Z = torch.zeros_like(Y)
+    print('id(Z):', id(Z))
+    Z[:] = X + Y
+    print('id(Z):', id(Z))
+
+    before = id(X)
+    X += Y
+    print('before:')
+    print(before)
+    print('X:')
+    print(X)
+    print(f'id(X) == before: {id(X) == before}')
 
 
 if __name__=='__main__':
